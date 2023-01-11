d19dc4e21acdeb62afd6127a27b0d0d79ab13514
yazan zidan <yazan.zidan@maf.ae>
2023-01-11 22:12:59 +0300

    adding input userdata and remaining tickets for the go conference

--- booking-app/main.go
+++ booking-app/main.go
@@ -6,16 +6,35 @@ func main() {
 	conferenceName := "Go Conference"
 	const conferenceTickets int = 50
 	var remainingTickets uint = 50
-
+	bookings := []string {}
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v conferenceTickets tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
-
-	var userName string
+    
+    
+	
+	var firstName string
+	var lastName  string
+	var email     string
 	var userTicktes int
 	//ask your for thier name
-	userName = "Tom"
-	userTicktes = 2
-	fmt.Printf("User %v booked %v tickets.\n", userName, userTicktes)
+	fmt.Println("Enter your first name:")
+    fmt.Scan(&firstName)
+	
+	fmt.Println("Enter your last name:")
+    fmt.Scan(&lastName)
 
+	fmt.Println("Enter your email:")
+    fmt.Scan(&email)
+	
+	fmt.Println("Enter number of tickets:")
+    fmt.Scan(&userTicktes)
+
+	remainingTickets = remainingTickets - uint(userTicktes)
+    bookings = append(bookings, firstName+ "" + lastName )
+	
+	fmt.Printf("Thank you %v %v for booking %v tickets. you will receive a confirmation email at %v\n", firstName, lastName, userTicktes, email)
+    fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+	fmt.Printf("These are all our booking : %v\n", bookings)
 }
