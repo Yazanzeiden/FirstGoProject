5c82a3518e2b5710f8f0cf8dec566076746b433a
yazan zidan <yazan.zidan@maf.ae>
2023-01-11 20:08:11 +0300

    adding user booking

--- booking-app/main.go
+++ booking-app/main.go
@@ -2,18 +2,20 @@ package main
 
 import "fmt"
 
-func main()  {	
-	var conferenceName = "Go Conference"
-	const conferenceTickets  = 50
-	var remainingTickets = 50
+func main() {
+	conferenceName := "Go Conference"
+	const conferenceTickets int = 50
+	var remainingTickets uint = 50
 
-	fmt.Println("Welcome to", conferenceName ,"booking application")
-	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets , "are still available")
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v conferenceTickets tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-     
+	var userName string
+	var userTicktes int
+	//ask your for thier name
+	userName = "Tom"
+	userTicktes = 2
+	fmt.Printf("User %v booked %v tickets.\n", userName, userTicktes)
 
 }
-
-
-
